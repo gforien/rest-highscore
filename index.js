3d875573c1b13231f948e8ac8d2cd85b2fc5a40e
Gabriel Forien <gforien@gmail.com>
2020-04-25 17:26:56 +0200

    done in 1h and lost 26min to a stupid bug :bug:

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 /***************************************
  *        Express configuration        *
  ***************************************/
-const PORT = process.env.PORT | 5000;
+const PORT = process.env.PORT || 5000;
 const express = require('express');
 const app = express();
 
 app
   .use(express.json())
   .use(express.urlencoded({extended: false}))
-  .use((res,req,next) => {
+  .use((req, res, next) => {
     console.log(req.method + ' ' + req.url + ' ' + JSON.stringify(req.body));
     next();
   })
@@ -16,10 +16,26 @@ app
 /*************************************
  *        Mongo configuration        *
  *************************************/
-const MOGODB_URI = process.env.MOGODB_URI;
+const MONGODB_URI = process.env.MONGODB_URI || '';
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
+const Schema = mongoose.Schema;
+const mongooseOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+}
 
+mongoose.connect(MONGODB_URI, mongooseOptions);
+mongoose.connection
+  .once('open', () => console.log('Connected to database'))
+  .on('error', (err) => console.error(err));
+
+
+let Highscores = mongoose.model(
+  'Highscores',
+  new Schema( {highscore: Number, username: String, datetime: Date}, {collection : 'highscores'}),
+);
 
 /************************
  *        routes        *
@@ -27,6 +43,76 @@ mongoose.Promise = global.Promise;
 app.get('/', (req, res) => {
   res.status(200).end('This is the front page');
 });
+
+app.get('/api/highscores', (req, res) => {
+  Highscores
+    .find()
+    .then(data => res.status(200).send(data))
+    .catch(err => {
+      console.error(err);
+      res.status(500).end('Server error');
+    });
+});
+
+app.get('/api/highscores/:id', (req, res) => {
+  let filter = {_id : req.params.id};
+
+  Highscores
+    .findOne(filter)
+    .then(data => res.status(200).send(data))
+    .catch(err => {
+      console.error(err);
+      res.status(500).end('Server error');
+    });
+});
+
+app.put('/api/highscores/:id', (req, res) => {
+  let filter = {_id : req.params.id};
+  let update = req.body;
+
+  Highscores
+    .findOneAndUpdate(filter, update, {new: true})
+    .then(() => res.status(200).end())
+    .catch(err => {
+      console.error(err);
+      res.status(500).end('Server error');
+    });
+});
+
+app.delete('/api/highscores/:id', (req, res) => {
+  const filter = {_id : req.params.id};
+
+  Highscores
+    .findOneAndDelete(filter)
+    .then(() => res.status(200).end())
+    .catch(err => {
+      console.error(err);
+      res.status(500).end('Server error');
+    });
+});
+
+app.post('/api/highscores', (req, res) => {
+  const newHighscore = new Highscores(req.body);
+
+  newHighscore
+    .save()
+    .then(() => res.status(200).end())
+    .catch(err => {
+      console.error(err);
+      res.status(500).end('Server error');
+    });
+});
+
+// 404 Route : keep at the bottom
+app.use((req, res) => {
+  res.status(404).end('404 Error : Page not found');
+});
+
+// 500 Route : keep at the bottom
+app.use((req, res) => {
+  res.status(500).end('500 Error : Server error');
+});
+
 
 module.exports = app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
